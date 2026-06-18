672f68882042792f39f691c3c095ad1139197ae5
Jesus <giljesusgilpantoja@gmail.com>
2026-06-18 17:27:01 -0400

    fix: actualizar origenes permitidos en CORS para Netlify

--- 3erPracticoWebIIBackend/index.js
+++ 3erPracticoWebIIBackend/index.js
@@ -9,8 +9,8 @@ const cors = require('cors');
 const path = require('path');
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
-    optionsSuccessStatus: 200
+  origin: '*',
+  optionsSuccessStatus: 200
 }));
 
 // Servir archivos subidos (fotos, banners, imágenes de posts)
